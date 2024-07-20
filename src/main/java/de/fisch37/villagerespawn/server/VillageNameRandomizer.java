c5a5dd612907222be8f5fb8e1504c7ca8f141823
Fisch37 <seracin4@gmail.com>
2024-07-20 21:15:08 +0200

    Village names are now deterministic

--- src/main/java/de/fisch37/villagerespawn/server/VillageNameRandomizer.java
+++ src/main/java/de/fisch37/villagerespawn/server/VillageNameRandomizer.java
@@ -4,17 +4,17 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.structure.StructureStart;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.random.Random;
+import net.minecraft.util.math.random.Xoroshiro128PlusPlusRandomImpl;
 
 import static de.fisch37.villagerespawn.VillageRespawn.MOD_ID;
 
 public class VillageNameRandomizer {
     public static final short VILLAGE_NAME_POOL_SIZE = 666;
-    private final static Identifier VILLAGE_RANDOMIZER = Identifier.of(MOD_ID, "village_name_random");
 
-    private final Random random;
+    private final long seed;
 
     public VillageNameRandomizer(ServerWorld world) {
-        random = world.getOrCreateRandom(VILLAGE_RANDOMIZER);
+        seed = world.getSeed();
     }
 
     public String getRandomName(StructureStart structure) {
@@ -23,11 +23,23 @@ public class VillageNameRandomizer {
     }
 
     private int getRandForStructure(StructureStart structure) {
-        // TODO: Use some kind of hash-like system to generate a "random" value.
-        //  Essentially, I want to base the name of a village off the world seed
-        //  and its unique identifier (i.e. the BlockPos of the structure).
-        //  This means village names are seed-dependent and therefore consistent
-        //  across users.
-        return random.nextBetween(0, VILLAGE_NAME_POOL_SIZE);
+        // Generates a deterministic village name.
+        // The aim here is to ensure that:
+        //  1. Village names are dependent on the world seed.
+        //  2. Village names are independent of their discovery time.
+        // This is accomplished by seeding a new random generator
+        // with the world seed and the position of the village structure.
+        return getRandForLong(structure
+                .getBoundingBox()
+                .getCenter()
+                .asLong()
+        );
+    }
+
+    private int getRandForLong(long pos) {
+        // I don't know shit about random distributions.
+        // I'm just hoping this doesn't create bias anywhere
+        long rawValue = new Xoroshiro128PlusPlusRandomImpl(seed, pos).next();
+        return (int) Math.abs(rawValue % VILLAGE_NAME_POOL_SIZE);
     }
 }
